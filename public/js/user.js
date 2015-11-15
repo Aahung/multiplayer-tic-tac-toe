79bb24a6df6f32b54db938affdc77989f3833f1a
AgnesMayYao <xuewenyao2-c@my.cityu.edu.hk>
2015-11-15 15:43:47 +0800

    Update user.js

--- public/js/user.js
+++ public/js/user.js
@@ -6,6 +6,8 @@
 function User(nickname, image, type) {
     this.nickname = nickname;
     this.image = image;
+    if (!this.image) this.image = "images/profile-icon-50.png";
+	else this.image="http://graph.facebook.com/"+this.image+"/picture?type=square";
     this.type = type;
 }
 
@@ -16,5 +18,11 @@ User.prototype.render = doT.compile($('#user-template').html());
 User.prototype.draw = function(userList) {
     var html = User.prototype.render(this);
     this.element = $(html);
+
+    // admin controllers
+    $(this.element).hover(function() {
+    	
+    });
+    
     $(userList).append(this.element);
 };
